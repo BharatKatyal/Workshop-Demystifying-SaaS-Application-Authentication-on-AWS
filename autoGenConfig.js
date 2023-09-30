818af052637eb7fd607b2678e424ae5fedd0cc75
EC2 Default User <ec2-user@ip-172-31-82-201.ec2.internal>
2023-09-30 08:50:25 +0000

    Finished labs 1-4

---
+++ autoGenConfig.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// this file is auto generated, do not edit it directly
+exports.default = {
+    "cognitoDomain": "auth-959328062626-us-east-1.auth.us-east-1.amazoncognito.com",
+    "region": "us-east-1",
+    "cognitoUserPoolId": "us-east-1_X1WZxkCWB",
+    "cognitoUserPoolAppClientId": "55d5jvbnr99a9466r24o70pa40",
+    "apiUrl": "https://eyvdv3qoa0.execute-api.us-east-1.amazonaws.com/prod/",
+    "appUrl": "https://d2os8lg6v4a5ug.cloudfront.net",
+    "uiBucketName": "externalidpdemo-uibucketb980636d-dsjsaeg6v8tx"
+};
